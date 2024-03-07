c0e617cd6488ed90278d4d4c2557021d7409819d
NS_PC-01\pulle <pulleyln@outlook.com>
2024-03-06 21:54:33 -0600

    Doors now tell you how to open them with a key press - need to add key press to open door, still opens automatically

--- Assets/Week-6/Scripts/DoorTrigger.cs
+++ Assets/Week-6/Scripts/DoorTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DoorTrigger : MonoBehaviour
 {
@@ -13,11 +14,16 @@ public class DoorTrigger : MonoBehaviour
     public bool isOpening;
     private float alpha;
 
+    [SerializeField] GameObject DoorInstructions;
+    private bool firstOpen;
+
     private void Awake()
     {
         origin = door.transform.position;
         target = origin + (Vector2.up * 2);
+        firstOpen = true;
     }
+
     private void Update()
     {
         //time to open door
@@ -29,17 +35,23 @@ public class DoorTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //tells the player how to open a door the first time they encounter one until the first time they open one
+        if (firstOpen) DoorInstructions.SetActive(true);
+
         PlayerController2D player = collision.GetComponent<PlayerController2D>();
+
         //checks if the collider is a player object and if the player has keys to be able to open the door
-        if (collision.tag == "Player" && player.GetKeys() > 0)
+        if (player.tag == "Player" && player.GetKeys() > 0)
         {
             isOpening = true;
-            Debug.Log("Player has a key");
+
             //get rid of player key
             player.UseKey();
             player.UpdateKeyText();
+
+            firstOpen = false; //never updates again, player knows how to open doors
         }
-        else Debug.Log("Player does not have a key");
+        //else Debug.Log("Player does not have a key");
     }
 
     //could get rid of to make doors stay open once unlocked
@@ -47,6 +59,7 @@ public class DoorTrigger : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         isOpening = false;
+        DoorInstructions.SetActive(false);
     }
 
 }
--- Assets/Week-6/Scripts/PlayerInputActions.cs
+++ Assets/Week-6/Scripts/PlayerInputActions.cs
@@ -53,6 +53,15 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Open"",
+                    ""type"": ""Button"",
+                    ""id"": ""788a9bd5-679f-47eb-8eec-f389dac61e85"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -273,6 +282,17 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""XR"",
                     ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0131bf6f-ecb6-44cd-92f9-9b0c96e83752"",
+                    ""path"": ""<Keyboard>/#(K)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Open"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -863,6 +883,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Open = m_Player.FindAction("Open", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -939,6 +960,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Open;
     public struct PlayerActions
     {
         private @PlayerInputActions m_Wrapper;
@@ -946,6 +968,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Open => m_Wrapper.m_Player_Open;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -964,6 +987,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Fire.started += instance.OnFire;
             @Fire.performed += instance.OnFire;
             @Fire.canceled += instance.OnFire;
+            @Open.started += instance.OnOpen;
+            @Open.performed += instance.OnOpen;
+            @Open.canceled += instance.OnOpen;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -977,6 +1003,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Fire.started -= instance.OnFire;
             @Fire.performed -= instance.OnFire;
             @Fire.canceled -= instance.OnFire;
+            @Open.started -= instance.OnOpen;
+            @Open.performed -= instance.OnOpen;
+            @Open.canceled -= instance.OnOpen;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -1162,6 +1191,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
+        void OnOpen(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
